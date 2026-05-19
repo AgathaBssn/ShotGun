d83a0b00fa166c8a78cb196151aa78e86a18ff7b
Agatha BASSON <agatha.basson@gmail.com>
2026-05-19 16:25:07 +0200

    add: random joker constructor implemented

--- src/Joker/Joker.java
+++ src/Joker/Joker.java
@@ -1,9 +1,25 @@
 package src.Joker;
 
+import java.util.Random;
+
 public class Joker {
     private ETypeJoker typeJoker;
 
-    public Joker(ETypeJoker typeJoker){
+    public Joker(ETypeJoker typeJoker) {
         this.typeJoker = typeJoker;
+    }
+
+    public Joker() {
+        this.typeJoker = randomEnum(ETypeJoker.class);
+    }
+
+    public static <T extends Enum<?>> T randomEnum(Class<T> clazz) {
+        Random random = new Random();
+        int x = random.nextInt(clazz.getEnumConstants().length);
+        return clazz.getEnumConstants()[x];
+    }
+
+    public ETypeJoker getTypeJoker() {
+        return typeJoker;
     }
 }
